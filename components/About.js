7dfdaa924ea89a69a9501e7044b957080ac9296e
Kyle Rehak <kylerehak@gmail.com>
2023-02-26 16:55:55 +0800

    Adjust reponsive grid

--- components/About.js
+++ components/About.js
@@ -6,8 +6,8 @@ const About = () => {
       id="about"
       className="w-full md:h-screen px-6 md:px-12 flex items-center py-16"
     >
-      <div className="max-w-[1240px] m-auto md:grid grid-cols-3 gap-8">
-        <div className="col-span-2">
+      <div className="max-w-[1240px] m-auto md:grid grid-cols-2 gap-8">
+        <div className="md:pr-2 lg:pr-4">
           <p className="text-xl tracking-widest uppercase text-[#5651e5]">
             About
           </p>
@@ -30,11 +30,11 @@ const About = () => {
             pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
             culpa qui officia deserunt mollit anim id est laborum.
           </p>
-          <p className="py-2 text-gray-600 underline cursor-pointer">
+          <p className="mb-6 md:mb-0 py-4 text-gray-600 underline cursor-pointer">
             Check out some of my latest projects
           </p>
         </div>
-        <div className="w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-200">
+        <div className="w-full h-auto m-auto  shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-200">
           <img
             className="rounded-xl"
             src="https://images.unsplash.com/photo-1553390774-b4822481c894?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=744&q=80"
