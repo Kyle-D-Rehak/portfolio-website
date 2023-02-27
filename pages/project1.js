c7af6e79cbd6dfd3c236e46380ac450cd36f34af
Kyle Rehak <kylerehak@gmail.com>
2023-02-27 12:49:26 +0800

    Create project page template

--- pages/project1.js
+++ pages/project1.js
@@ -1,6 +1,9 @@
 import Image from "next/image";
 import React from "react";
 import project4 from "../public/assets/projects/project4.png";
+import { RiRadioButtonFill } from "react-icons/ri";
+import { HiChevronDoubleLeft } from "react-icons/hi";
+import Link from "next/link";
 
 const project1 = () => {
   return (
@@ -17,6 +20,55 @@ const project1 = () => {
         <div className="absolute top-[70%] max-w-[1240px] w-full left-[50%] translate-x-[-50%] translate-y-[-50%] text-[#ecf0f3] px-6 z-10">
           <h2 className="py-2">Project 1</h2>
           <h3>React JS / Next JS / Tailwind</h3>
+        </div>
+      </div>
+
+      <div className="max-w-[1240px] mx-auto p-6 grid md:grid-cols-5 gap-8 pt-8">
+        <div className="col-span-4">
+          <p className="text-lg tracking-widest uppercase text-[#5651e5]">
+            Project
+          </p>
+          <h2 className="pb-4 text-gray-700">Overview</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <button className="px-8 py-2 mt-4 mr-8">Demo</button>
+          <button className="px-8 py-2 mt-4">Code</button>
+        </div>
+        <div className="col-span-4 md:col-span-1 shadow-xl shadow-gray-400 rounded-xl p-4">
+          <div className="p-2">
+            <p className="text-center font-bold pb-2">Technologies</p>
+            <div className="grid grid-cols-3 md:grid-cols-1">
+              <p className="text-gray-600 py-2 flex items-center">
+                <RiRadioButtonFill className="pr-1" /> React
+              </p>
+              <p className="text-gray-600 py-2 flex items-center">
+                <RiRadioButtonFill className="pr-1" /> Next JS
+              </p>
+              <p className="text-gray-600 py-2 flex items-center">
+                <RiRadioButtonFill className="pr-1" /> Tailwind
+              </p>
+              <p className="text-gray-600 py-2 flex items-center">
+                <RiRadioButtonFill className="pr-1" /> Firebase
+              </p>
+              <p className="text-gray-600 py-2 flex items-center">
+                <RiRadioButtonFill className="pr-1" /> JavaScript
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-center py-6 col-span-5">
+          <Link href="/#projects">
+            <div className="rounded-full shadow-lg shadow-gray-400 p-4 cursor-pointer hover:scale-110 ease-in duration-200">
+              <HiChevronDoubleLeft size={30} className="text-[#5651e5]" />
+            </div>
+          </Link>
         </div>
       </div>
     </div>
